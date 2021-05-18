cb5aa80f0442634c2266ae9fe3ed41299802be07
Jose Solis <j.solisgom@gmail.com>
2021-05-18 19:02:43 -0400

    Inserción de datos en la tabla Products a través de Seeders

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,7 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\Product;
+use App\Models\Subcategory;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class ProductFactory extends Factory
 {
@@ -21,8 +23,22 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+
+        $name = $this->faker->sentence(2);
+        $subcategory = Subcategory::all()->random();
+        $category = $subcategory->category;
+        $brands = $category->brands->random();
+        $quantity = ($subcategory->color) ? null : 15;
+
         return [
-            //
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomElement([19.990, 49.990, 99.000]),
+            'subcategory_id' => $subcategory->id,
+            'brand_id' => $brands,
+            'quantity' => $quantity,
+            'status' => 2
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(SubcategorySeeder::class);
+        $this->call(ProductSeeder::class);
 
     }
 }
