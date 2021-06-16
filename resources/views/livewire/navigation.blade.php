25883cdf70367af64519d3132b92fb7394c341f9
Jose Solis <j.solisgom@gmail.com>
2021-06-15 22:21:00 -0400

    Adición de ícono de carro de compras e implementación de diseño del menú

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,10 @@
-<header class="bg-trueGray-700">
+<style>
+    #navigation-menu{
+        height: calc(100vh - 4rem);
+    }
+</style>
+
+<header class="bg-trueGray-700 sticky top-0">
     <div class="container flex items-center h-16">
         <a class="flex flex-col items-center justify-center px-4 bg-white-bg-opacity-25 text-white cursor-pointer font-semibold h-full" href="">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -13,7 +19,7 @@
 
         @livewire('search')
 
-        <div class="ml-3 relative">
+        <div class="mx-6 relative">
             @auth
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -63,6 +69,32 @@
             @endauth
         </div>
 
+        @livewire('dropdown-cart')
+
         {{-- <x-search size="35" color="white"></x-search> --}}
     </div>
+
+    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+        <div class="container h-full">
+            <div class="grid grid-cols-4 h-full">
+                <ul class="bg-white">
+                    @foreach ($categories as $category)
+                        <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                                <span class="flex justify-center w-9">
+                                    {!!$category->icon!!}
+                                </span>
+                                {{$category->name}}
+                            </a>
+
+                            <div class="bg-red-500 absolute w-3/4 h-full top-0 right-0">
+
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="col-span-3 bg-gray-100"></div>
+            </div>
+        </div>
+    </nav>
 </header>
